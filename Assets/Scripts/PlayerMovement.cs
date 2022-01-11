e87fc5654e0b681d78e1738aeaae4f1943715e2a
LucRohmer <89489290+LucRohmer@users.noreply.github.com>
2022-01-11 15:41:49 -0800

    Completed Quiz 2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private Quaternion rotation;
+    private bool isWalking;
+
+    [SerializeField] private float turnSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    //Animator event
+    private void OnAnimatorMove()
+    {
+      
+      
+    }
+}
